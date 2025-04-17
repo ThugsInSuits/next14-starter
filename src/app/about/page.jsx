4598d393077e025fe8180e6eb3593d9ab24422ae
ThugsInSuits <1036023528@qq.com>
2025-04-18 07:09:55 +0800

    about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,13 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <div className={styles.imgContainer} >
+        <Image src="https://images.pexels.com/photos/31023837/pexels-photo-31023837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill/>
+      </div>
+    </div>
   )
 }
 
