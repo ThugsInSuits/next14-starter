f199eba9e90e3fbe0fb75ca83123e4708258880d
ThugsInSuits <1036023528@qq.com>
2025-04-26 17:50:01 +0800

    complete contact page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,20 +5,29 @@ const AboutPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h2>About Agency</h2>
-        <h2>We create digital ideas that are bigger, bolder, braver and better.</h2>
-        <p>
-        We create digital ideas that are bigger, bolder, braver and better. We believe in good ideas flexibility and precission We’re world’s Our Special Team best consulting & finance solution provider. Wide range of web and software development services.
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h2 className={styles.title}>We create digital ideas that are bigger, bolder, braver and better.</h2>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We believe in good ideas flexibility and precission We’re world’s Our Special Team best consulting & finance solution provider. Wide range of web and software development services.
         </p>
-      </div>
-      <div className={styles.boxes}>
-        <div className={styles.box}>
-          <h1>10 K++</h1>
-          <p>Year of experience</p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K++</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K++</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K++</h1>
+            <p>Year of experience</p>
+          </div>
         </div>
       </div>
+
       <div className={styles.imgContainer}>
-        
+        <Image className={styles.img}src="/about.png" alt="About Image" fill />
       </div>
     </div>
   )
